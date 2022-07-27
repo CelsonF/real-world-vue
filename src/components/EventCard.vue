0f05685ef6cb44c2892a193717ca03d3e4450c65
CelsonF <celsondev@outlook.com>
2022-07-27 01:26:58 -0300

    add configurations my workspace

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="card-title">{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+data() {
+  return {
+    msg: "Card title"
+  }
+},
+}
+</script>
